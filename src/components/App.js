be4b40eed7dce37926ab525c7c3055b063fd3d58
rowaxl <rowaxl0@gmail.com>
2019-02-17 23:05:35 +0900

    connect with blank components

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-// dummy
-const PageOne = () => {
-    return (<div>
-        PageOne
-        <Link to="/two">To Page2</Link>
-        </div>);
-};
-
-const PageTwo = () => {
-    return (<div>
-        PageTwo
-        <Link to="/">To Page1</Link>
-        </div>);
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
 
 class App extends Component {
     render() {
@@ -22,8 +12,11 @@ class App extends Component {
             <div>
                 <BrowserRouter>
                     <div>
-                        <Route path="/" exact component={PageOne} />
-                        <Route path="/two" component={PageTwo} />
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/show" exact component={StreamShow} />
+                        <Route path="/streams/edit" exact component={StreamEdit} />
+                        <Route path="/streams/delete" exact component={StreamDelete} />
                     </div>
                 </BrowserRouter>
             </div>
