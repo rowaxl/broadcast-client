8fad52bf2b379c40b70fe19a68c62172a7020aba
rowaxl <rowaxl0@gmail.com>
2019-02-20 21:43:04 +0900

    little more progress

--- src/components/OAuth.js
+++ src/components/OAuth.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
 
 class OAuth extends Component {
+    state = { isSignedIn: false };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: process.env.CLIENT_ID,
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
             });
         });
     }
 
+    renderAuthButton() {
+        if (!this.state.isSignedIn) {
+            return <div>SignIn</div>
+        } else {
+            return <div>SignOut</div>
+        }
+    }
+
     render() {
         return (
-            <div className="item">OAuth</div>
+            <div className="item">{this.renderAuthButton()}</div>
         )
     }
 };
