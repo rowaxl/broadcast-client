1cef4f5dd41fe8e20d985cf9b31735f6f20832bf
rowaxl <rowaxl0@gmail.com>
2019-02-23 21:56:17 +0900

    prototye form validate message

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,25 +2,52 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
-    renderInput({ input, label}) {
+    renderInput({ input, label, meta }) {
         return (
             <div className='field'>
                 <label>{label}</label>
                 <input {...input} />
+                <div>{meta.error}</div>
             </div>
         );
+    };
+
+    onSubmit(formValue) {
+        console.log(formValue);
     }
 
     render() {
         return (
-            <form className="ui form">
-                <Field name="title" component={this.renderInput} label='Enter Title' />
-                <Field name="description" component={this.renderInput} label='Enter Description' />
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title"
+                    component={this.renderInput}
+                    label='Enter Title'
+                />
+                <Field
+                    name="description"
+                    component={this.renderInput}
+                    label='Enter Description'
+                />
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
 };
 
+const validate = formValues => {
+    const errors = {};
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate: validate
 })(StreamCreate);
