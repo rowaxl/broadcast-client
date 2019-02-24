ecb18026a05dcbca57fad75f730196cdb26cbdec
rowaxl <rowaxl0@gmail.com>
2019-02-24 22:16:50 +0900

    add fetch list

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -1,4 +1,4 @@
-import { omit } from 'lodash';
+import { omit, mapKeys } from 'lodash';
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -9,6 +9,8 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            return { ...state, ...mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
         case CREATE_STREAM:
         case EDIT_STREAM:
