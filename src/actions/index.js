12a8b7426d6850fdb639f87338f46f3c774e89ec
rowaxl <rowaxl0@gmail.com>
2019-02-24 21:51:48 +0900

    add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT } from './types';
+import {
+    SIGN_IN,
+    SIGN_OUT,
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+} from './types';
 
 export const signIn = (userId) => {
     return {
@@ -15,6 +23,16 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-    const response = streams.post('/streams', formValues);
-    dispatch({ type: 'CREATE_STREAM', payload: response.data });
-}
+    const response = await streams.post('/streams', formValues);
+    dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, payload: response.data });
+};
