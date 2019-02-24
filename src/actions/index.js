b6af993f451bf13ba1d2f8bcffa0ad3bf0c2c3c6
rowaxl <rowaxl0@gmail.com>
2019-02-24 21:53:38 +0900

    add last apis

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,3 +36,13 @@ export const fetchStream = id => async dispatch => {
     const response = await streams.get(`/streams/${id}`);
     dispatch({ type: FETCH_STREAM, payload: response.data });
 };
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+    const response = await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: response.data });
+};
