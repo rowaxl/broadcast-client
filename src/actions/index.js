86ae4156653bfe643c161657682cff19f5ab611d
rowaxl <rowaxl0@gmail.com>
2019-02-25 22:40:27 +0900

    add history

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,8 +24,9 @@ export const signOut = () => {
 
 export const createStream = formValues => async (dispatch, getState) => {
     const { userId } = getState().auth;
-    const response = await streams.post('/streams', {...formValues, userId });
+    const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    
 };
 
 export const fetchStreams = () => async dispatch => {
