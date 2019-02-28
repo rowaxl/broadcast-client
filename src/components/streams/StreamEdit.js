8b86a2c8a33ba74c0ae7e234f4012e5354955bf1
rowaxl <rowaxl0@gmail.com>
2019-02-28 22:03:17 +0900

    implement mapStateToProps

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class StreamEdit extends Component {
     render() {
@@ -9,4 +10,8 @@ class StreamEdit extends Component {
     }
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps)(StreamEdit);
