6bd4e3f0b0cbc57b4ddf7b32b332000c5c38bae7
rowaxl <rowaxl0@gmail.com>
2019-03-01 21:56:54 +0900

    connect fetch stream action to edit component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 class StreamEdit extends Component {
+    componentDidMount = () => {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
     render() {
         console.log(this.props);
+
+        if (!this.props.stream) {
+            return (
+                <div class="ui segment">
+                    <div class="ui active inverted dimmer">
+                        <div class="ui indeterminate text loader">Preparing Files</div>
+                    </div>
+                </div>
+            );
+        }
+
         return (
-            <div>StreamEdit:{this.props.match.params.id}</div>
+            <div>
+                {this.props.stream.title}
+            </div>
         );
     }
 };
@@ -14,4 +32,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
