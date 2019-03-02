43d04febc4ad9f7b2c366cd6cdb46c8417d6d40a
rowaxl <rowaxl0@gmail.com>
2019-03-02 21:42:24 +0900

    add cancel function

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
+import history from '../history';
 
 class Modal extends Component {
     render() {
         return createPortal(
-            <div className="ui dimmer modals visible active">
-                <div className="ui standard modal visible active">
-                    This is Modal.
+            <div
+                onClick={() => history.push('/')}
+                className="ui dimmer modals visible active"
+            >
+                <div
+                    onClick={(e) => e.stopPropagation()}
+                    className="ui standard modal visible active"
+                >
+                    <div className="header">Delete Stream</div>
+                    <div className="content">
+                        Are sure you want to delete this stream?
+                    </div>
+                    <div className="actions">
+                        <button className="ui negative button">Delete</button>
+                        <button
+                            onClick={() => history.push('/')}
+                            className="ui button"
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </div>
-            </div>,
-            document.querySelector('#modal')
+            </div>
+            ,document.querySelector('#modal')
         );
     };
 };
